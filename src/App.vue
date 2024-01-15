ff066eb7cc6520f6e360f90e9878badb06e27bc2
Waydj <farhminait@gmail.com>
2024-01-15 09:47:33 +0300

    RandomGenerator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import RandomGenerator from "./components/RandomGenerator.vue";
 import ToDoList from "./components/ToDoList.vue";
 </script>
 
 <template>
   <ToDoList />
+  <RandomGenerator />
 </template>
