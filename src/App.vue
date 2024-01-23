1dcb58401c6f1fa243ef227611983b4aecd3f667
waydj <farhminait@gmail.com>
2024-01-23 17:28:07 +0200

    Form Validation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AccordionApp from "./components/AccordionApp.vue";
 import CalculatorApp from "./components/CalculatorApp.vue";
+import FormValidation from "./components/FormValidation.vue";
 import RandomGenerator from "./components/RandomGenerator.vue";
 import ToDoList from "./components/ToDoList.vue";
 </script>
@@ -10,4 +11,5 @@ import ToDoList from "./components/ToDoList.vue";
   <RandomGenerator />
   <CalculatorApp />
   <AccordionApp />
+  <FormValidation />
 </template>
